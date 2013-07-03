9a126c7a4d24e172b79560e6fd9f12287a356c4a
Christian Schubert <mail@apexo.de>
2013-07-03 13:55:20 +0200

    more cffi interfaces

---
+++ hdr/socket.py
@@ -0,0 +1,93 @@
+HDR = """
+typedef %(IN_PORT_T)s in_port_t;
+typedef uint32_t socklen_t;
+
+typedef struct iovec
+{
+	void *iov_base;			/* Pointer to data.  */
+	size_t iov_len;			/* Length of data.  */
+};
+
+typedef struct sockaddr
+{
+	unsigned short int sa_family;	/* Common data: address family and length.  */
+	char sa_data[14];		/* Address data.  */
+};
+
+typedef struct sockaddr_storage
+{
+	unsigned short int ss_family;	/* Address family, etc.  */
+	%(SS_ALIGNTYPE)s __ss_align;	/* Force desired alignment.  */
+	char __ss_padding[%(SS_PADSIZE)d];
+};
+
+typedef uint32_t in_addr_t;
+typedef struct in_addr
+{
+	in_addr_t s_addr;
+};
+
+
+typedef struct in6_addr
+{
+	union
+	{
+		uint8_t	__u6_addr8[16];
+		uint16_t __u6_addr16[8];
+		uint32_t __u6_addr32[4];
+	} __in6_u;
+};
+
+typedef struct sockaddr_in
+{
+	unsigned short int sin_family;
+	in_port_t sin_port;		/* Port number.  */
+	struct in_addr sin_addr;	/* Internet address.  */
+
+	/* Pad to size of `struct sockaddr'.  */
+	unsigned char sin_zero[%(SIN_PADSIZE)d];
+};
+
+/* Ditto, for IPv6.  */
+typedef struct sockaddr_in6
+{
+	unsigned short int sin6_family;
+	in_port_t sin6_port;		/* Transport layer port # */
+	uint32_t sin6_flowinfo;		/* IPv6 flow information */
+	struct in6_addr sin6_addr;	/* IPv6 address */
+	uint32_t sin6_scope_id;		/* IPv6 scope-id */
+};
+
+typedef struct msghdr
+{
+	void *msg_name;		/* Address to send to/receive from.  */
+	socklen_t msg_namelen;	/* Length of address data.  */
+
+	struct iovec *msg_iov;	/* Vector of data to send/receive into.  */
+	size_t msg_iovlen;	/* Number of elements in the vector.  */
+
+	void *msg_control;	/* Ancillary data (eg BSD filedesc passing). */
+	size_t msg_controllen;	/* Ancillary data buffer length.
+					!! The type should be socklen_t but the
+					definition of the kernel is incompatible
+					with this.  */
+	int msg_flags;		/* Flags on received message.  */
+};
+
+ssize_t sendmsg(int sockfd, const struct msghdr *msg, int flags);
+ssize_t recvmsg(int sockfd, struct msghdr *msg, int flags);
+
+"""
+
+def get_cdef(ffi):
+	SS_ALIGNTYPE = "unsigned long int"
+	SS_PADSIZE = 128 - 2 * ffi.sizeof(SS_ALIGNTYPE)
+	IN_PORT_T = "uint16_t"
+	SIN_PADSIZE = 16 - ffi.sizeof(IN_PORT_T) - ffi.sizeof("unsigned short int") - 4
+
+	return HDR % {
+		"SS_ALIGNTYPE": SS_ALIGNTYPE,
+		"SS_PADSIZE": SS_PADSIZE,
+		"IN_PORT_T": IN_PORT_T,
+		"SIN_PADSIZE": SIN_PADSIZE,
+	}
---
+++ sockmsg.py
@@ -0,0 +1,78 @@
+import socket
+from gnutls_ffi import ffi
+
+def addrtuple_to_sockaddr(family, addr):
+	if family not in (socket.AF_INET, socket.AF_INET6):
+		raise ValueError("unsupported address family: %r" % (family,))
+	binaddr = socket.inet_pton(family, addr[0])
+	if family == socket.AF_INET:
+		if len(addr) != 2:
+			raise ValueError("unsupported IN address: %r" % (addr,))
+		name = ffi.new("struct sockaddr_in*")
+		#name.sin_family = socket.htons(family)
+		name.sin_family = family
+		name.sin_port = socket.htons(addr[1])
+		ffi.buffer(ffi.addressof(name.sin_addr))[:] = binaddr
+	else:
+		if not 2 <= len(addr) <= 4:
+			raise ValueError("unsupported IN6 address: %r" % (addr,))
+		name = ffi.new("struct sockaddr_in6*")
+		#name.sin6_family = socket.htons(family)
+		name.sin6_family = family
+		name.sin6_port = socket.htons(addr[1])
+		name.sin6_flowinfo = 0 if len(addr) < 3 else socket.htonl(addr[2])
+		ffi.buffer(ffi.addressof(name.sin6_addr))[:] = binaddr
+		name.sin6_scope_id = 0 if len(addr) < 4 else socket.htonl(addr[3])
+	return name
+
+def addrtuple_to_name(family, addr):
+	sockaddr = addrtuple_to_sockaddr(family, addr)
+	return ffi.buffer(sockaddr)[:]
+
+def name_to_addrtuple(name):
+	assert isinstance(name, bytes)
+	assert len(name) in (14, 28)
+	temp = ffi.new("unsigned char[]", name)
+	sa = ffi.cast("struct sockaddr*", temp)
+	return sockaddr_to_addrtuple(sa)
+
+def sockaddr_to_addrtuple(sa):
+	#family = socket.ntohs(sa.sa_family)
+	family = sa.sa_family
+	if family == socket.AF_INET:
+		sin = ffi.cast("struct sockaddr_in*", sa)
+		return socket.inet_ntop(family, ffi.buffer(ffi.addressof(sin.sin_addr))[:]), socket.ntohs(sin.sin_port)
+	elif family == socket.AF_INET6:
+		sin6 = ffi.cast("struct sockaddr_in6*", sa)
+		return socket.inet_ntop(family, ffi.buffer(ffi.addressof(sin6.sin6_addr))[:]), socket.ntohs(sin6.sin6_port), socket.ntohl(sin6.sin6_flowinfo), socket.ntohl(sin6.sin6_scope_id)
+	else:
+		raise ValueError("unsupported address family: %r" % (family,))
+
+class TargetedMessage(object):
+	__slots__ = ["name", "msg", "_name", "_iov"]
+
+	def __init__(self, name):
+		assert isinstance(name, bytes) and len(name) in (14, 28), (repr(name), len(name))
+		self._name = ffi.new("unsigned char[]", name)
+		self._iov = ffi.new("struct iovec[]", 1)
+		self.name = ffi.buffer(self._name)[:]
+		self.msg = ffi.new("struct msghdr*")
+		self.msg.msg_name = self._name
+		self.msg.msg_namelen = len(name)
+		self.msg.msg_control = ffi.cast("void*", 0)
+		self.msg.msg_controllen = 0
+		self.msg.msg_flags = 0
+
+	def send(self, fd):
+		return lib.sendmsg(fd, self.msg, 0)
+
+	def set_vec(self, iov, iovlen):
+		self.msg.msg_iov = iov
+		self.msg.msg_iovlen = iovlen
+
+	def set(self, base, len):
+		print("msg.set, %r, %r" % (base, len))
+		self.msg.msg_iov = self._iov
+		self.msg.msg_iovlen = 1
+		self._iov[0].iov_base = base
+		self._iov[0].iov_len = len
